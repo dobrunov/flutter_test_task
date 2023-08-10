ca3c2a6b9c6d8b39c0085930a2d16572c3e8d063
Evgeniy Dobrunov <e.dobrunov@gmail.com>
2023-08-10 14:22:27 +0300

    add new font

--- lib/constants/text_styles.dart
+++ lib/constants/text_styles.dart
@@ -5,15 +5,17 @@ import 'color_styles.dart';
 class TextStyles {
   //
   static TextStyle nameTextStyle22Black = const TextStyle(
-    fontSize: 22.0,
+    fontSize: 30.0,
     fontWeight: FontWeight.w700,
     color: ColorStyles.black,
+    fontFamily: 'PermanentMarker',
   );
   //
   static TextStyle profileNameTextStyle28Black = const TextStyle(
-    fontSize: 22.0,
+    fontSize: 28.0,
     fontWeight: FontWeight.w700,
     color: ColorStyles.black,
+    fontFamily: 'PermanentMarker',
   );
   //
   static TextStyle emailTextStyle16Grey = const TextStyle(
